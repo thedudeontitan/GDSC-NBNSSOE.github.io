2d5b07deb95309499e04b8c96b587757530b02e6
Aryan Kathawale <78982932+kiritocode1@users.noreply.github.com>
2022-09-06 06:57:35 +0000

    FIX : fixing the svelte pipeline and setup

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"GDSC-NBNSSOE.GITHUB.IO/",
+  base:"/",
   plugins: [svelte()]
 })
