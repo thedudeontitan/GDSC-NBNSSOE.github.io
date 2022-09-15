28cd247b700f3a6b20726a6f95e1b11539e976bd
thedudeontitan <97180942+thedudeontitan@users.noreply.github.com>
2022-09-15 17:13:08 +0530

    initialized team

--- src/pages/team/index.jsx
+++ src/pages/team/index.jsx
@@ -1,15 +1,82 @@
 import React from "react";
 import styles from "./index.module.css";
-
+import avatar from "./../../assets/avatar.png"
+import {SiLinkedin} from "react-icons/si"
 
 export default function Team(){
 
 
 
     return (
-            <div className={styles.root}>
-                <p>Initial</p>
+        
+        <section id="team">
+            <div>
+
             </div>
+        
+            <div className={styles.gridcontainer}>
+            <div className={styles.card}>
+                <img src={avatar} alt="avatar"/>
+                <div class="container">
+                    <h4><b>JOHN DOE</b></h4>
+                    <p>xyz lead</p>
+                    <a href="https://linkedin.com">
+                    <SiLinkedin className={styles.linkedinlogo}/>
+                    </a>
+                </div>
+            </div>
+            <div className={styles.card}>
+                <img src={avatar} alt="avatar"/>
+                <div class="container">
+                    <h4><b>JOHN DOE</b></h4>
+                    <p>xyz lead</p>
+                    <a href="https://linkedin.com">
+                    <SiLinkedin className={styles.linkedinlogo}/>
+                    </a>
+                </div>
+            </div>
+            <div className={styles.card}>
+                <img src={avatar} alt="avatar"/>
+                <div class="container">
+                    <h4><b>JOHN DOE</b></h4>
+                    <p>xyz lead</p>
+                    <a href="https://linkedin.com">
+                    <SiLinkedin className={styles.linkedinlogo}/>
+                    </a>
+                </div>
+            </div>
+            <div className={styles.card}>
+                <img src={avatar} alt="avatar"/>
+                <div class="container">
+                    <h4><b>JOHN DOE</b></h4>
+                    <p>xyz lead</p>
+                    <a href="https://linkedin.com">
+                    <SiLinkedin className={styles.linkedinlogo}/>
+                    </a>
+                </div>
+            </div>
+            <div className={styles.card}>
+                <img src={avatar} alt="avatar"/>
+                <div class="container">
+                    <h4><b>JOHN DOE</b></h4>
+                    <p>xyz lead</p>
+                    <a href="https://linkedin.com">
+                    <SiLinkedin className={styles.linkedinlogo}/>
+                    </a>
+                </div>
+            </div>
+            <div className={styles.card}>
+                <img src={avatar} alt="avatar"/>
+                <div class="container">
+                    <h4><b>JOHN DOE</b></h4>
+                    <p>xyz lead</p>
+                    <a href="https://linkedin.com">
+                    <SiLinkedin className={styles.linkedinlogo}/>
+                    </a>
+                </div>
+            </div>
+            </div>
+        </section>
 
     )
 }
